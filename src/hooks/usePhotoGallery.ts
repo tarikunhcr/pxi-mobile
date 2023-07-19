aab936318ab9bd584499963a67a6d4c2daa5820f
tarikjrh <tarikjarrah@hotmiail.com>
2023-07-19 16:29:28 +0300

    fdg

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -31,10 +31,9 @@ import { Capacitor } from "@capacitor/core"
 const PHOTO_STORAGE = "photos"
 export function usePhotoGallery() {
     const [photos, setPhotos] = useState<UserPhoto[]>([])
-    Camera.checkPermissions().then((e) => {
-        alert(e.camera)
-    })
+
     useEffect(() => {
+        Camera.requestPermissions()
         const loadSaved = async () => {
             const { value } = await Preferences.get({ key: PHOTO_STORAGE })
 
